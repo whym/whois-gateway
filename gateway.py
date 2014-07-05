286228746d48d8ada02b289d0aaa4c6d85efa018
Yusuke Matsubara <whym@whym.org>
2014-07-06 01:43:29 +0900

    Lookup

--- gateway.py
+++ gateway.py
@@ -1,7 +1,11 @@
 #! /usr/bin/env python
+import sys
+sys.path.insert(0, '/data/project/whois/local/lib/python2.7/site-packages')
 
 import cgitb
 import cgi
+from ipwhois import IPWhois
+import json
 
 if __name__ == '__main__':
         providers = {
@@ -13,21 +17,41 @@ if __name__ == '__main__':
         cgitb.enable(display=0, logdir="/data/project/whois/logs")
         form = cgi.FieldStorage()
         ip = form.getfirst("ip", "")
-        provider = form.getfirst("provider", "")
+        provider = form.getfirst("provider", "").upper()
+        fmt = form.getfirst("format", "html").lower()
+        doLookup = form.getfirst("lookup", "")
 
+        result = ''
+        if doLookup != '':
+                query = IPWhois(ip)
+                result = query.lookup()
+        
         if providers.has_key(provider):
                 print "Location: %s" % providers[provider](ip)
                 print ""
                 exit()
 
+        if fmt == 'json' and doLookup:
+                print "Content-type: text/plain"
+                print ""
+                print result
+                exit()
+                
         print "Content-type: text/html"
         print ""
         print '''
 <style type="text/css">
-li a { display: inline-block; padding: .2em 0; font: normal normal 120%sans-serif; }
+li a { display: inline-block; padding: .2em 0; font: normal normal sans-serif; }
+#result { border: 1px solid; }
 </style>
 <title>Whois Gateway</title>
-<body><h1>Whois Gateway</h1>
+<body>
+<h1>Whois Gateway</h1>
+'''
+        if doLookup:
+                print '<div id="result"><pre>%s</pre></div>' % json.dumps(result, indent=4)
+        print '''
+<h2>External links</h2>
 <ul>
 '''
         for (name,q) in sorted(providers.items()):
@@ -36,7 +60,9 @@ li a { display: inline-block; padding: .2em 0; font: normal normal 120%sans-seri
 
 print '''
 <h2>Usage</h2>
-<dl><dt>http://tools.wmflabs.org/whois/IPADDRESS</dt>
+<dl><dt>http://tools.wmflabs.org/whois/IPADDRESS/lookup</dt>
+<dd>Whois result</dd>
+<dt>http://tools.wmflabs.org/whois/IPADDRESS</dt>
 <dd>List of links to regional databases</dd>
 <dt>http://tools.wmflabs.org/whois/IPADDRESS/redirect/NAME</dt>
 <dd>Redirect to a search result page provided by NAME.<dd>
